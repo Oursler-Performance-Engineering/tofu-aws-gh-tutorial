b9b27a0ba46ab6dd24709dc7c5bf91cb6dca81a5
Justin Oursler <justin.oursler@protonmail.com>
2025-03-26 11:09:31 -0500

    Add required providers to terraform.

--- github_role_stack/backend.tf
+++ github_role_stack/backend.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0.0"
 
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     region  = "us-east-2"
     bucket  = "example-dev-github-role-stack-state"
