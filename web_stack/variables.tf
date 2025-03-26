ecf8fde61ffc98dd507bcba7f89c2870bcfd4389
Justin Oursler <justin.oursler@protonmail.com>
2025-03-26 11:07:13 -0500

    Add VPC and EC2 instance.

--- web_stack/variables.tf
+++ web_stack/variables.tf
@@ -3,3 +3,15 @@ variable "region" {
   type = string
   default = "us-east-2"
 }
+
+variable "cidr" {
+  description = "VPC CIDR"
+  type = string
+  default = "10.0.0.0/16"
+}
+
+variable "num_azs" {
+  description = "Desired number of AZs to use in a region."
+  type        = number
+  default     = 1
+}
