5b55f4c8de3d5bf655606e23cd7586a7c5f6eb08
Justin Oursler <justin.oursler@protonmail.com>
2025-03-28 11:13:09 -0500

    Fixing web stack backend.

--- web_stack/backend.tf
+++ web_stack/backend.tf
@@ -1,20 +1,11 @@
 terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
     region  = "us-east-2"
-    bucket  = "example-dev-web-stack-state"
-    key     = "terraform.tfstate"
+    bucket  = "example-dev-oursler-backend-state"
+    key     = "web_stack.tfstate"
     profile = ""
     encrypt = "true"
 
-    dynamodb_table = "example-dev-web-stack-state-lock"
+    dynamodb_table = "example-dev-oursler-backend-state-lock"
   }
 }
